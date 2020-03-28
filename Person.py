9b223ac545525567590f298070533a72d96c0576
DHuryn <dnaiel.v.huryn@gmail.com>
2020-03-28 17:03:47 -0400

    Add files via upload

--- Person.py
+++ Person.py
@@ -16,17 +16,17 @@ avg_sicktime = 14
 work_increase_in_chance = 2
 
 class Person:
-    def _init_(self, age, home, status, position, still_working, time_sick):
+    def __init__(self, age, home, status, position, still_working):
         self.age = age
         self.home = home
         self.status = status
         self.position = position
         self.still_working = still_working
-        time_sick = 0
+        self.time_sick = 0
         
     def move(self, home, position, still_working, age):
         if(still_working):
-            self.position = self.position + avg_speed *work_increase_in_chance * avg_age / age * self.direct(home, position)
+            self.position = self.position + avg_speed * work_increase_in_chance * avg_age / age * self.direct(home, position)
         else:
             self.position = self.position + avg_speed * avg_age / age * self.direct(home, position)
 
@@ -51,7 +51,7 @@ class Person:
             if random.randrange(0,1) < self.time_sick / avg_sicktime:
                 self.status = "Immune"
             
-                        
+Person(1, (1,1), "Healthy", (1,1), 0)                       
             
         
     
