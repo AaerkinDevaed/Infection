5d15b8633a6777d5cf1c08ad93bf8746c1b58747
Mack Hutsell <mack.hutsell@gmail.com>
2020-03-28 20:47:57 -0500

    It work

--- simulation.py
+++ simulation.py
@@ -5,30 +5,38 @@ import random
 import matplotlib.pyplot as plt
 
 def main():
-    houston = City(3662, 3662)
+    houston = City(1500, 300)
 
-    simulation_length = 365
+    simulation_length = 30
     time = np.arange(0,simulation_length)
 
     immune = [0]
     healthy = [len(houston.people_list) - 1]
-    infected = [1] 
-
+    infected = [1]
 
     for t in time[1:]:
-        houston.next_day
+        houston.next_day()
+        houston.change_infected()
+        # houston.print_pos(t)
         immune.append(houston.num_immune)
         healthy.append(houston.num_healthy)
         infected.append(houston.num_infected)
 
+    dead = [0]
+    for i in immune[1:]:
+        dead.append(0.03*i)
+    for i, d in enumerate(dead):
+        immune[i] - d
+
+    print(dead)
     plt.plot(time, immune, color = "green", label = "Immune")
     plt.plot(time, infected, color = "red", label = "Infected")
     plt.plot(time, healthy, color = "blue", label = "Healthy")
+    plt.plot(time, dead, color = "black", label = "Dead")
     plt.legend()
     plt.xlabel("# of Days")
     plt.ylabel("# of People")
     plt.show()
-
 
 if __name__ == "__main__":
     main()
