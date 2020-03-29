f8e0cc1055ebe03599f9739272ab0cc9a31ca14e
AaerkinDevaed <daniil.v.huryn@emory.edu>
2020-03-29 00:36:13 -0400

    Generalized sim

--- simulation.py
+++ simulation.py
@@ -1,26 +1,25 @@
-from Person import Person
+
 from city import City
 import numpy as np
-import random
 import matplotlib.pyplot as plt
 
-def main():
-    houston = City(1500, 300)
+def sim(pop, dens):
+    rand_city = City(pop, dens)
 
-    simulation_length = 30
+    simulation_length = 40
     time = np.arange(0,simulation_length)
 
     immune = [0]
-    healthy = [len(houston.people_list) - 1]
+    healthy = [len(rand_city.people_list) - 1]
     infected = [1]
 
     for t in time[1:]:
-        houston.next_day()
-        houston.change_infected()
+        rand_city.next_day()
+        rand_city.change_infected()
         # houston.print_pos(t)
-        immune.append(houston.num_immune)
-        healthy.append(houston.num_healthy)
-        infected.append(houston.num_infected)
+        immune.append(rand_city.num_immune)
+        healthy.append(rand_city.num_healthy)
+        infected.append(rand_city.num_infected)
 
     dead = [0]
     for i in immune[1:]:
@@ -39,5 +38,4 @@ def main():
     plt.show()
 
 if __name__ == "__main__":
-    main()
-
+    sim(500,300)
