dd967abbb536ace4e60e7b64d5ebfe38c78896a0
Mack Hutsell <mack.hutsell@gmail.com>
2020-04-11 20:26:06 -0500

    Added quarantine, city type variation, some minor changes to sick person behavior, fixed housing placement.

--- simulation.py
+++ simulation.py
@@ -13,25 +13,31 @@ def main():
     immune = [0]
     healthy = [len(houston.people_list) - 1]
     infected = [1]
+    quarantined = [0]
 
     for t in time[1:]:
         houston.next_day()
         houston.change_infected()
-        # houston.print_pos(t)
         immune.append(houston.num_immune)
         healthy.append(houston.num_healthy)
         infected.append(houston.num_infected)
+        quarantined.append(houston.num_quarantined)
 
     dead = [0]
     for i in immune[1:]:
         dead.append(0.03*i)
     for i, d in enumerate(dead):
         immune[i] - d
+    print_info(dead, time, immune, infected, healthy, quarantined)
 
+
+
+def print_info(dead, time, immune, infected, healthy, quarantined):
     print(dead)
     plt.plot(time, immune, color = "green", label = "Immune")
     plt.plot(time, infected, color = "red", label = "Infected")
     plt.plot(time, healthy, color = "blue", label = "Healthy")
+    plt.plot(time, quarantined, color = "yellow", label = "Quarantined")
     plt.plot(time, dead, color = "black", label = "Dead")
     plt.legend()
     plt.xlabel("# of Days")
