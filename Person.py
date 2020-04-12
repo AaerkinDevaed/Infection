e694e8561a16742f73f66efb3bd137fc9d2b57ec
Mack Hutsell <mack.hutsell@gmail.com>
2020-04-12 18:27:29 -0500

    Adds icus and markets, fixes counting of immune and dead, makes dead list  GUI friendly

--- Person.py
+++ Person.py
@@ -12,20 +12,42 @@ import numpy as np
 radius = .05
 inf_prob = .8
 avg_sicktime = 14
+avg_markettime = 5
+perc_to_icu = 0.03
 
 class Person:
-    def __init__(self, age, home, status, position, still_working, edge_size):
+    def __init__(self, age, home, status, position, still_working, icu_worker, edge_size, local_market, local_icu):
         self.age = age
         self.side_length = edge_size
         self.home = home
         self.status = status
         self.position = position
+        self.local_market = local_market
+        self.local_icu = local_icu
         self.still_working = still_working
+        self.icu_worker = icu_worker
         self.time_sick = 0
+        self.time_to_last_market = 0
 
     def move(self, avg_speed, work_increase_in_chance):
+        if(self.icu_worker == True):
+            if self.position == self.local_icu:
+                self.positon = self.home
+            else:
+                self.position == self.local_icu
+            return
+
+        if(self.position == self.local_market):
+            self.position == self.home
+            return
+
         if(self.status == "Quarantined"):
             return
+
+        if random.random() < self.time_to_last_market / avg_markettime:
+            self.position = self.local_market
+            return
+
         if(self.time_sick > 8):
             self.still_working = False
 
@@ -74,8 +96,8 @@ class Person:
                         count += 0.2
 
             stay_healthy = (1 - inf_prob)**count
-            if stay_healthy > .8:
-                stay_healthy = .8
+            if stay_healthy < .2:
+                stay_healthy = .2
             if random.random() > stay_healthy:
                 self.status = "Newly Infected"
 
@@ -89,5 +111,9 @@ class Person:
 
         elif self.status == "Quarantined":
             self.time_sick += 1
+            if random.random() < perc_to_icu:
+                self.position = self.local_icu
             if random.random() < self.time_sick / avg_sicktime:
                 self.status = "Immune"
+                if self.position == self.local_icu:
+                    self.position = self.home
